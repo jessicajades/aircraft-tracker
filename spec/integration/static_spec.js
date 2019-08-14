80fa59bde1e08fc60c720b82905a72007d129b40
Jessica Shepherd <jessicashepherd@Jessicas-MacBook-Pro.local>
2019-08-14 14:02:01 -0700

    homepage

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,15 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+    describe("GET /", () => {
+        it("should return status code 200 and have 'aircraft tracker' in the response", done => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain("AIRCRAFT TRACKER");
+                done();
+            });
+        });
+    });
+});
