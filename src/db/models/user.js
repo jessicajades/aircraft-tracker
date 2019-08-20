8d71dcea6a9d6907f271c94cfcf98db2f30e7841
Jessica Shepherd <jessicashepherd@Jessicas-MacBook-Pro.local>
2019-08-20 11:34:48 -0700

    added sign up/in/out

---
+++ src/db/models/user.js
@@ -0,0 +1,34 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define(
+        "User",
+        {
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    isEmail: { msg: "must be a valid email" }
+                }
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            role: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0
+            }
+        },
+        {}
+    );
+    User.associate = function(models) {
+        // associations can be defined here
+    };
+
+    User.prototype.isAdmin = function() {
+        return this.role === 1;
+    };
+
+    return User;
+};
